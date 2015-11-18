f2c1651f31d03fc2fbdcb23bedcaa558c3991c97
Amanda Iaria <amanda.iaria@gmail.com>
2015-11-18 14:28:58 -0500

    codeship
    Testing out cadetship with something that is a work in progress

--- tcp_cal.php
+++ tcp_cal.php
@@ -6,6 +6,13 @@
 	Description: Calulator to figure out how much someone can save when they stop smoking
 
 */
+	# Set php version through phpenv. 5.3, 5.4, 5.5 & 5.6 available
+phpenv local 5.5
+# Install extensions through Pecl
+# yes yes | pecl install memcache
+
+# Install dependencies through Composer
+composer install --prefer-source --no-interaction
 
 	require_once("tcp_cal_scripts.php");
 	require_once("tcp_cal_shortcode.php");
